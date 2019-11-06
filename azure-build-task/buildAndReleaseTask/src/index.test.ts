18465b302a2cca929e71646eaa60511ee3daed62
Ryan Joy <atxryan@gmail.com>
2019-11-06 13:20:50 -0800

    code coverage wip

--- azure-build-task/buildAndReleaseTask/src/index.test.ts
+++ azure-build-task/buildAndReleaseTask/src/index.test.ts
@@ -1,10 +1,22 @@
 
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
+import * as path from 'path';
+
 
 describe('Sample task tests', function () {
 
     it('should succeed with simple inputs', function() {
-        console.log('done');
+    
+        let tp = path.join(__dirname, 'success.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+    
+        tr.run();
+        console.log(tr.succeeded);
+        expect(tr.succeeded).toBeTruthy();
+        expect(tr.warningIssues.length).toEqual(0);
+        expect(tr.errorIssues.length).toEqual(0);
+        console.log(tr.stdout);
+        expect(tr.stdout.indexOf('Hello human')).toBeGreaterThanOrEqual(0)
     });
 
     it('it should fail if tool returns 1', function() {
