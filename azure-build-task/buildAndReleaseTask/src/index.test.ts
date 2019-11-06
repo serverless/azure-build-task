6dff3612d4b97f9539fa6ed86cc3338c79c8dd92
Ryan Joy <atxryan@gmail.com>
2019-11-06 11:17:56 -0800

    test coverage wip

---
+++ azure-build-task/buildAndReleaseTask/src/index.test.ts
@@ -0,0 +1,13 @@
+
+import * as ttm from 'azure-pipelines-task-lib/mock-test';
+
+describe('Sample task tests', function () {
+
+    it('should succeed with simple inputs', function() {
+        console.log('done');
+    });
+
+    it('it should fail if tool returns 1', function() {
+        // Add failure test here
+    });    
+});
